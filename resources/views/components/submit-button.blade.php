f44e669e0ba143401cb1158b291a93239c10e7fc
Wilbert B. Anadia <17-1-00359@vsu.edu.ph>
2026-04-30 20:29:20 +0800

    fixed button guard click at once  v2

--- resources/views/components/submit-button.blade.php
+++ resources/views/components/submit-button.blade.php
@@ -1,3 +1,12 @@
 <button
     {{ $attributes->merge(['type' => 'submit']) }}
+    x-data="{ busy: false }"
+    @click="if (busy || $el.hasAttribute('data-submitting')) { $event.preventDefault(); $event.stopImmediatePropagation(); return; } busy = true;"
+    @submit.window.capture="
+        if ($el.closest('form') && $event.target === $el.closest('form')) {
+            busy = true;
+            $el.disabled = true;
+            $el.setAttribute('data-submitting', 'true');
+        }
+    "
 >{{ $slot }}</button>
