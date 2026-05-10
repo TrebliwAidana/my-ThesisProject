cefb52faeb7175c02370068306a14eee0f268646
Wilbert B. Anadia <17-1-00359@vsu.edu.ph>
2026-05-11 04:02:22 +0800

    fixed db seeder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,26 @@
 echo "Discovering packages..."
 php artisan package:discover --ansi
 
+echo "Clearing previous cache..."
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Running seeders..."
+php artisan db:seed --force
+
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
+echo "Caching views..."
+php artisan view:cache
 
 echo "Starting PHP-FPM..."
 php-fpm -D
